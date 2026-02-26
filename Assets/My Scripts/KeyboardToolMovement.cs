29a30fd1cda4e3956ae91cb2463b62ef7f2c20c3
EMKwasam <samuelkwa1@gmail.com>
2026-02-26 19:49:55 +0800

    Allowed switching between open and closed MicroKiap model

--- Assets/My Scripts/KeyboardToolMovement.cs
+++ Assets/My Scripts/KeyboardToolMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine.InputSystem;
 public class KeyboardToolMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
-    [SerializeField] private float rotationSpeed = 20f;
-    [SerializeField] private float verticalRotationSpeed = 20f;
+    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private float verticalRotationSpeed = 1f;
 
     private Rigidbody rb;
     private Vector2 movementInput;
