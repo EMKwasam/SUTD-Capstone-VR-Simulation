24cbcecf6b9afa9d1f240e3e2efc1f63fce742f3
EMKwasam <samuelkwa1@gmail.com>
2026-04-09 15:48:16 +0800

    Corrected switch values

--- Assets/My Scripts/ColliderPickupJoystickSwitch.cs
+++ Assets/My Scripts/ColliderPickupJoystickSwitch.cs
@@ -235,7 +235,7 @@ public class ColliderPickupJoystickSwitch : MonoBehaviour
             return false;
         }
 
-        return switchControl.ReadValue() < switchOnThreshold;
+        return switchControl.ReadValue() >= switchOnThreshold;
     }
 
     private void HandleSwitchTurnedOn()
